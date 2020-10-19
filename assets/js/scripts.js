f6cb2c4b076b6c6bc42e3ac31f506c2b975794d1
rmlimpin <ronel.limpin@gmail.com>
2020-10-19 22:42:09 +0800

    LOTTA front-end
    i did a lot of things particularly
    - the navbar
    - added some images on the portfolio
    - minor changes to make it look sweet

---
+++ assets/js/scripts.js
@@ -0,0 +1,28 @@
+// bootstrap
+$(document).ready(function(){
+	$('[data-toggle="tooltip"]').tooltip();   
+});
+
+// transparent navbar which change opacity when scrolled
+$(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
+
+function myFunction(imgs) {
+  // Get the expanded image
+  var expandImg = document.getElementById("expandedImg");
+  // Get the image text
+  var imgText = document.getElementById("imgtext");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
+}
